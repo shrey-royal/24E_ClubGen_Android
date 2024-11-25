771c0e3eedbabf1e1f073e9d5ecae4bd6035e59b
Shrey K <srkkadia@gmail.com>
2024-11-25 19:18:25 +0530

    Button

--- FirstApp/app/src/main/java/com/company/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/company/firstapp/MainActivity.java
@@ -1,19 +1,42 @@
 package com.company.firstapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText n1, n2;
+    private TextView tv_res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        n1 = findViewById(R.id.edt_n1);
+        n2 = findViewById(R.id.edt_n2);
+        Button btn = findViewById(R.id.btn_calculate);
+        tv_res = findViewById(R.id.tv_res);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int a = Integer.parseInt(n1.getText().toString());
+                    int b = Integer.parseInt(n2.getText().toString());
+                    tv_res.setText(String.valueOf(a + b));
+                } catch (NumberFormatException e) {
+                    tv_res.setText("Error: Please input both numbers");
+                }
+            }
+        });
     }
 }
