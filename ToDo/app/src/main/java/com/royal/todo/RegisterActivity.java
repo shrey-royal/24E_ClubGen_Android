0ad8e20d8205ff6dcd57b66e8fd771a460d6e613
Shrey K <srkkadia@gmail.com>
2025-04-01 18:15:27 +0530

    Atempt #3

--- ToDo/app/src/main/java/com/royal/todo/RegisterActivity.java
+++ ToDo/app/src/main/java/com/royal/todo/RegisterActivity.java
@@ -1,22 +1,27 @@
 package com.royal.todo;
 
-import android.credentials.GetCredentialException;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CancellationSignal;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential;
-import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import androidx.credentials.Credential;
 import androidx.credentials.CredentialManager;
@@ -24,6 +29,7 @@ import androidx.credentials.CredentialManagerCallback;
 import androidx.credentials.CustomCredential;
 import androidx.credentials.GetCredentialRequest;
 import androidx.credentials.GetCredentialResponse;
+import androidx.credentials.exceptions.GetCredentialException;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -36,13 +42,6 @@ public class RegisterActivity extends AppCompatActivity {
     private Button btnGoogleSignIn, btnGoogleSignOut;
     private TextView userName, userEmail;
     private ImageView userImage;
-
-//    private final ActivityResultLauncher<Intent> signInLauncher =
-//            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-//                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-//                    handleSignInResult(result.getData());
-//                }
-//            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,35 +68,25 @@ public class RegisterActivity extends AppCompatActivity {
     private void signIn() {
         GetCredentialRequest req = new GetCredentialRequest.Builder()
                 .addCredentialOption(new GetGoogleIdOption.Builder()
-                        .setFilterByAuthorizedAccounts(true)
-                        .setServerClientId(getString(R.string.gcm_defaultSenderId))
+                        .setFilterByAuthorizedAccounts(false)
+                        .setServerClientId(getString(R.string.default_web_client_id))
                         .build())
                 .build();
 
-        executorService.execute(() -> {
-            try {
-                credentialManager.getCredentialAsync(
-                        this,
-                        req,
-                        null,
-                        null,
-                        new CredentialManagerCallback<GetCredentialResponse, GetCredentialException>() {
-                            @Override
-                            public void onResult(GetCredentialResponse res) {
-                                handleSignInResult(res);
-                            }
+        credentialManager.getCredentialAsync(
+                this, req, null, executorService,
+                new CredentialManagerCallback<GetCredentialResponse, GetCredentialException>() {
+                    @Override
+                    public void onResult(GetCredentialResponse res) {
+                        Log.d("TAG", "res");
+                        handleSignInResult(res);
+                    }
 
-                            @Override
-                            public void onError(@NonNull GetCredentialException e) {
-                                Log.e(TAG, "Sign in failed: " + e.getMessage());
-                                Toast.makeText(RegisterActivity.this, "Sign in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
+                    @Override
+                    public void onError(@NonNull GetCredentialException e) {
+                        Log.e(TAG, "Sign in failed: " + e.getMessage());
+                    }
                 });
-            } catch (Exception e) {
-                Log.e(TAG, "Error occurred while getting credentials: " + e.getMessage());
-                Toast.makeText(RegisterActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     private void handleSignInResult(GetCredentialResponse res) {
@@ -106,9 +95,40 @@ public class RegisterActivity extends AppCompatActivity {
         if (credential instanceof CustomCredential) {
             if (GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL.equals(credential.getType())) {
                 GoogleIdTokenCredential googleIdTokenCredential = GoogleIdTokenCredential.createFrom(((CustomCredential) credential).getData());
+
+                String idToken = googleIdTokenCredential.getIdToken();
+                Log.d("TOKEN", idToken);
+                AuthCredential authCredential = GoogleAuthProvider.getCredential(idToken, null);
+                mAuth.signInWithCredential(authCredential)
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(this, "Sign-in successful!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.e(TAG, "Sign-in failed!");
+                            }
+                        });
             }
         }
     }
 
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+        Toast.makeText(this, "Signed Out!", Toast.LENGTH_SHORT).show();
+    }
 
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            userName.setText(user.getDisplayName());
+            userEmail.setText(user.getEmail());
+            Glide.with(this).load(user.getPhotoUrl()).into(userImage);
+            btnGoogleSignIn.setVisibility(View.GONE);
+            btnGoogleSignOut.setVisibility(View.VISIBLE);
+        } else {
+            userName.setText("User Name");
+            userEmail.setText("Email");
+            btnGoogleSignIn.setVisibility(View.VISIBLE);
+            btnGoogleSignOut.setVisibility(View.GONE);
+        }
+    }
 }
